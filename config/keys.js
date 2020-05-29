021219121e91b2469808daa02a7528547e0f32c5
Justin Miller <justinmiller171@gmail.com>
2020-05-29 01:40:22 -0700

    updated mongo key

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://justin:SamJepp398@cluster0-vyvhf.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+    mongoURI: 'mongodb+srv://justin:rlA6wVdktF702oAY@cluster0-vyvhf.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
 }
